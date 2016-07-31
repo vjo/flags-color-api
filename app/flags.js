89ae71e56e7e617c707cd41653a5f7962b84a3f9
Victor Jolissaint <vjolissaint@twitter.com>
2016-07-31 10:30:42 +0100

    Move flags function in their own module

---
+++ app/flags.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const DIR = './data/';
+const EXT = '.json';
+
+const returnFlag = (name, response) => {
+  const file = name + EXT;
+  const path = DIR + name + EXT;
+
+  fs.readdir(DIR, (err, files) => {
+    if (files.indexOf(file) === -1) {
+      response.send({ err: 'not found' });
+    } else {
+      fs.readFile(path, (err, data) => {
+        const json = JSON.parse(data.toString());
+        response.send(json);
+      });
+    }
+  });
+};
+
+const returnAllFlags = (response) => {
+  let json = [];
+
+  fs.readdir(DIR, (err, files) => {
+    for (let i in files) {
+      let path = dir + files[i];
+
+      fs.readFile(path, (err, data) => {
+        json.push(JSON.parse(data.toString()));
+
+        if (i == files.length - 1) {
+          response.send(json);
+        }
+      });
+    }
+  });
+};
+
+module.exports = {
+  returnAllFlags,
+  returnFlag
+};
--- app/index.js
+++ app/index.js
@@ -1,19 +1,18 @@
 'use strict';
 
 const express = require('express');
-const fs = require('fs');
+const flags = require('./flags');
 
 const app = express();
 const port = 3000;
 
 app.get('/flags', (request, response) => {
-  returnAllFlags(response);
+  flags.returnAllFlags(response);
 });
 
 app.get('/flags/:country_code', (request, response) => {
   const { country_code } = request.params;
-
-  returnFlag(country_code.toString(), response);
+  flags.returnFlag(country_code.toString(), response);
 });
 
 app.listen(port, (err) => {
@@ -23,40 +22,3 @@ app.listen(port, (err) => {
 
   console.log(`Server is listening on ${port}`);
 });
-
-const returnFlag = (name, response) => {
-  const dir = './data/';
-  const ext = '.json';
-  const file = name + ext;
-  const path = dir + name + ext;
-
-  fs.readdir(dir, (err, files) => {
-    if (files.indexOf(file) === -1) {
-      response.send({ err: 'not found' });
-    } else {
-      fs.readFile(path, (err, data) => {
-        const json = JSON.parse(data.toString());
-        response.send(json);
-      });
-    }
-  });
-};
-
-const returnAllFlags = (response) => {
-  const dir = './data/';
-  let json = [];
-
-  fs.readdir(dir, (err, files) => {
-    for (let i in files) {
-      let path = dir + files[i];
-
-      fs.readFile(path, (err, data) => {
-        json.push(JSON.parse(data.toString()));
-
-        if (i == files.length - 1) {
-          response.send(json);
-        }
-      });
-    }
-  });
-};
